e5ada9829ee12143c3272461b6f931061e813ae9
Mrbanano <6666alvaro666@gmail.com>
2022-05-22 21:24:37 -0500

    CustomHook del carrito

---
+++ src/hook/useInitialState.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import initialState from '../initialState';
+
+export const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  const addToCard = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+
+  const RemoveFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((items) => items.id !== payload.id),
+    });
+  };
+
+  return {
+    addToCard,
+    RemoveFromCart,
+  };
+};
